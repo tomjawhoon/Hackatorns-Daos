e27c84aff4db19e2137b34b38a14e7519138b6ab
phirathatfin <phirathat.ji@gmail.com>
2023-06-17 13:13:07 +0700

    updatre deal info

--- scripts/createDeal.ts
+++ scripts/createDeal.ts
@@ -1,10 +1,9 @@
 import { ContractTransaction } from "ethers";
 import hre, { ethers } from "hardhat";
-import { getAddressList, saveAddresses } from "../utils/address";
+import { getAddressList } from "../utils/address";
 import CID from "cids";
 
 export default async function main() {
-  const [deployer] = await ethers.getSigners();
   const addressList = await getAddressList(hre.network.name);
 
   const DealClientAddr = addressList.DealClient;
@@ -13,23 +12,25 @@ export default async function main() {
   const dealClient = DealClient.attach(DealClientAddr);
 
   const cid =
-    "baga6ea4seaqkxmkazzwmkcfe5bxwxxkyd5n2vyeindbj7nf2xxgnf7lw327rqei";
+    "baga6ea4seaqkxmkazzwmkcfe5bxwxxkyd5n2vyeindbj7nf2xxgnf7lw327rqei"; // Update Me Key -> Piece CID
 
   const cidHexRaw = new CID(cid).toString("base16").substring(1);
 
+  const pieceSize = 1048576; // Update Me Key -> Piece Size
+  const carSize = 682740; // Update Me -> CAR Size
+  const locationRef =
+    "https://data-depot.lighthouse.storage/api/download/download_car?fileId=e000fca4-1af4-475a-a7b5-5615d047461e.car"; // Update Me Key -> URL
+
   const cidHex = "0x" + cidHexRaw;
-  const pieceSize = 1048576;
   const verified = false;
   const label = cidHex;
   const startEpoch = 655000;
-  const endEpoch = 658000;
+  const endEpoch = 660000;
   const storagePricePerEpoch = 0;
   const providerCollateral = 0;
   const clientCollateral = 0;
   const extraParamsVersion = 1;
-  const locationRef =
-    "https://data-depot.lighthouse.storage/api/download/download_car?fileId=e000fca4-1af4-475a-a7b5-5615d047461e.car";
-  const carSize = 682740;
+
   const skipIpniAnnounce = false;
   const removeUnsealedCopy = false;
   const extraParamsV1 = [
@@ -53,11 +54,9 @@ export default async function main() {
     extraParamsV1,
   ];
 
-  //send a transaction to call makeDealProposal() method
   const transaction = await dealClient.makeDealProposal(DealRequestStruct);
   const transactionReceipt = await transaction.wait();
 
-  //listen for DealProposalCreate event
   const event = transactionReceipt.events[0].topics[1];
   console.log("Complete! Event Emitted. ProposalId is:", event);
 }
